a69e67121adda76746a53c9df51f2e9da7ffc540
pavel <makovpavel@rambler.ru>
2021-09-01 07:23:39 +0300

    надо сгруппировать по цене

--- data.py
+++ data.py
@@ -1,6 +1,10 @@
 from pymongo import MongoClient
 from pprint import pprint
 import pandas as pd
+import xlsxwriter
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 pd.options.display.max_columns = 6
 
@@ -9,8 +13,23 @@ data_m = db.avito['мужские часы']
 
 
 data = pd.DataFrame(data_m.find({}))
+data['views'] = data['views'][data['views']>=0].astype(np.int64)
+data['last'] = data['last'].astype(np.int64)
+
+
+print()
+
+views_in_day = data['views'][data['views']>=0]/data['last'][data['last']>0]
+data['views_in_day'] = views_in_day
+data['price'][data['price']== ' Бесплатно  '] = -2
+data['price'][data['price']== ' Цена не указана  '] = -1
+
+
+
+plt.bar(data['last'], data['views_in_day'])
+sns.barplot(data['price'], data['views_in_day'])
 
 
 
 
-pprint(data)
+data.to_excel('мужские часы авито 1_09_21.xlsx')
